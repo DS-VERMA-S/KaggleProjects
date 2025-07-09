50f8dc6f64a1a20fe3ab1cf755caf2da3914f121
Sachin Verma <vermasachin723@yahoo.com>
2025-07-09 22:23:54 +0530

    initial commit

---
+++ MakeDataCount/parse_pdf_data.py
@@ -0,0 +1,19 @@
+__version__ = "0.1.0"
+__auther__ =  "sachin verma"
+__email__ = "verma.sachin.ds@gmail.com"
+
+
+import os 
+import re
+import pandas as pd
+import docling
+
+
+class AcademicDocParser():
+    """Class to parse academic documents and extract data"""
+
+    def __init__(self):
+        pass
+
+    def parse_pdf(self, file_path):
+        
